88c1937cdd051a5c486b1fa7e9e025d69770bc60
royer_q <quentin.royer@epitech.eu>
2014-11-30 16:38:48 +0100

    partie1

--- charon/etape1/FunctionSignature.hpp
+++ charon/etape1/FunctionSignature.hpp
@@ -1,48 +1,34 @@
 #pragma once
 
-
-/*
-** ETAPE I.1
-** Voilà ça tiens en une ligne, j'ai demandé à payan parce que j'avais des doutes et il a fait pareil.
-** Je sais pas pourquoi dans le sujet il parle de spécialiser le template, je l'ai fait juste en dessous au cas où mais bon...
-*/
-
-#include <iostream>
+template <typename T>
+struct							FunctionSignature;
 
 template <typename T>
-struct		FunctionSignature
+struct							FunctionSignature <T(void)>
 {
-  typedef T	*type;
+	typedef T					(*type)();
 };
 
-/*
-** Après tests, les surcharges ci dessous ne changent pas le comportement du code
-** Je pensais au début que ca empecherait d'utiliser le template avec des fonctions
-** de plus de 4 arguments puisque c'est ce qu'ils semblent demander mais il n'en
-** est rien.
-*/
-/*
 template <typename T, typename U>
-struct		FunctionSignature <T(U)>
+struct							FunctionSignature <T(U)>
 {
-  typedef T	(*type)(U);
+	typedef T					(*type)(U);
 };
 
 template <typename T, typename U, typename V>
-struct		FunctionSignature <T(U, V)>
+struct							FunctionSignature <T(U, V)>
 {
-  typedef T	(*type)(U, V);
+	typedef T					(*type)(U, V);
 };
 
 template <typename T, typename U, typename V, typename W>
-struct		FunctionSignature <T(U, V, W)>
+struct							FunctionSignature <T(U, V, W)>
 {
-  typedef T	(*type)(U, V, W);
+	typedef T					(*type)(U, V, W);
 };
 
 template <typename T, typename U, typename V, typename W, typename X>
-struct		FunctionSignature <T(U, V, W, X)>
+struct							FunctionSignature <T(U, V, W, X)>
 {
-  typedef T	(*type)(U, V, W, X);
+	typedef T					(*type)(U, V, W, X);
 };
-*/
--- charon/etape2/Function.hpp
+++ charon/etape2/Function.hpp
@@ -1,78 +1,241 @@
 #pragma once
 
-/*
-** ETAPE I.2
-** Ca marche pour les pointeurs sur fonction
-** J'ai pas encore trouve pour les callables
-** J'ai pas testé avec boost::bind
-*/
-
-// base class
+/* BASE CLASS */
 
 template <typename T>
-class		Function : public T
+class				Function : public T
 {
+public:
+	Function()
+	{
+	}
 
+	Function(const T &rhs)
+	{
+	}
+
+	Function					&operator=(const T &rhs)
+	{
+		return (*this);
+	}
 };
 
-// specialisation pour 0 argument (void)
+/* 0 ARG SPECIALIZATION */
 
 template <typename T>
-class		Function <T()>
+class				Function <T(void)>
 {
-  typedef T	(*type)();
+	struct			IFunctor
+	{
+		virtual T operator()() = 0;
+		virtual ~IFunctor() {}
+	};
+
+	template <typename F>
+	struct			Functor : public IFunctor
+	{
+		F			func;
+
+		Functor(F _func) : func(_func)
+		{
+		}
+		virtual T	operator()()
+		{
+			return (func());
+		}
+	};
 
 public:
-  Function(type func)
-  {
-    _func = func;
-  }
+	template <typename F>
+	Function(F f)
+	{
+		func = new Functor<F>(f);
+	}
+	~Function()
+	{
+		delete (func);
+	}
+	T				operator()()
+	{
+		return ((*func)());
+	};
 
-  ~Function()
-  {
-  }
-
-  Function	&operator=(type func)
-  {
-    _func = func;
-    return (*this);
-  }
-
-  T		operator()()
-  {
-    return (_func());
-  }
 private:
-  type		_func;
+	IFunctor		*func;
 };
 
-// specialisation pour 1 argument
+/* 1 ARG SPECIALIZATION */
 
 template <typename T, typename U>
-class							Function <T(U)>
+class				Function <T(U)>
 {
-  typedef T						(*type)(U);
+	struct			IFunctor
+	{
+		virtual T operator()(U) = 0;
+		virtual ~IFunctor() {}
+	};
+
+	template <typename F>
+	struct			Functor : public IFunctor
+	{
+		F			func;
+
+		Functor(F _func) : func(_func)
+		{
+		}
+		virtual T	operator()(U arg)
+		{
+			return (func(arg));
+		}
+	};
 
 public:
-  Function(type func)
-  {
-    _func = func;
-  }
+	template <typename F>
+	Function(F f)
+	{
+		func = new Functor<F>(f);
+	}
+	~Function()
+	{
+		delete (func);
+	}
+	T				operator()(U arg)
+	{
+		return ((*func)(arg));
+	};
 
-  ~Function()
-  {
-  }
-
-  Function					&operator=(type func)
-  {
-    _func = func;
-    return (*this);
-  }
-
-  T							operator()(U arg)
-  {
-    return (_func(arg));
-  }
 private:
-  type						_func;
+	IFunctor		*func;
+};
+
+/* 2 ARGS SPECIALIZATION */
+
+template <typename T, typename U, typename V>
+class				Function <T(U, V)>
+{
+	struct			IFunctor
+	{
+		virtual T operator()(U, V) = 0;
+		virtual ~IFunctor() {}
+	};
+
+	template <typename F>
+	struct			Functor : public IFunctor
+	{
+		F			func;
+
+		Functor(F _func) : func(_func)
+		{
+		}
+		virtual T	operator()(U arg1, V arg2)
+		{
+			return (func(arg1, arg2));
+		}
+	};
+
+public:
+	template <typename F>
+	Function(F f)
+	{
+		func = new Functor<F>(f);
+	}
+	~Function()
+	{
+		delete (func);
+	}
+	T				operator()(U arg1, V arg2)
+	{
+		return ((*func)(arg1, arg2));
+	};
+
+private:
+	IFunctor		*func;
+};
+
+/* 3 ARGS SPECIALIZATION */
+
+template <typename T, typename U, typename V, typename W>
+class				Function <T(U, V, W)>
+{
+	struct			IFunctor
+	{
+		virtual T operator()(U, V, W) = 0;
+		virtual ~IFunctor() {}
+	};
+
+	template <typename F>
+	struct			Functor : public IFunctor
+	{
+		F			func;
+
+		Functor(F _func) : func(_func)
+		{
+		}
+		virtual T	operator()(U arg1, V arg2, W arg3)
+		{
+			return (func(arg1, arg2, arg3));
+		}
+	};
+
+public:
+	template <typename F>
+	Function(F f)
+	{
+		func = new Functor<F>(f);
+	}
+	~Function()
+	{
+		delete (func);
+	}
+	T				operator()(U arg1, V arg2, W arg3)
+	{
+		return ((*func)(arg1, arg2, arg3));
+	};
+
+private:
+	IFunctor		*func;
+};
+
+/* 4 ARGS SPECIALIZATION */
+
+template <typename T, typename U, typename V, typename W, typename X>
+class				Function <T(U, V, W, X)>
+{
+	struct			IFunctor
+	{
+		virtual T operator()(U, V, W, X) = 0;
+		virtual ~IFunctor() {}
+	};
+
+	template <typename F>
+	struct			Functor : public IFunctor
+	{
+		F			func;
+
+		Functor(F _func) : func(_func)
+		{
+		}
+		virtual T	operator()(U arg1, V arg2, W arg3, X arg4)
+		{
+			return (func(arg1, arg2, arg3, arg4));
+		}
+	};
+
+public:
+	template <typename F>
+	Function(F f)
+	{
+		func = new Functor<F>(f);
+	}
+	~Function()
+	{
+		delete (func);
+	}
+	T				operator()(U arg1, V arg2, W arg3, X arg4)
+	{
+		return ((*func)(arg1, arg2, arg3, arg4));
+	};
+
+private:
+	IFunctor		*func;
 };
